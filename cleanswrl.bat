ac1ef08ec847f6bdfa636e8ed2f82e7dda854542
netroby <netroby@netroby.com>
2011-03-12 16:56:31 +0800

    stop and restart chrome

--- cleanswrl.bat
+++ cleanswrl.bat
@@ -2,7 +2,10 @@
 set FL="C:\Documents and Settings\Administrator\Local Settings\Application Data\Google\Chrome\User Data\Default\Extensions\caehdcpeofiiigpdhbabniblemipncjj\SwitchyAuto.pac"
 if exist %FL% (
         del %FL%
-        echo 请重新启动chrome浏览器
+        echo 切换列表已清除
+        echo 重新启动chrome浏览器
+        taskkill /f /im chrome.exe 
+        start chrome
         ) else (
             echo 文件不存在无需删除
             )
